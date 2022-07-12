0ec8ab792ea74ef8ca9ab26cb0bab5a55111b36f
Ahmed-Sellami <ahmedsellami526@gmail.com>
2022-07-12 22:34:19 +0100

    Add STATIC_ROOT to settings.py

--- timein/settings.py
+++ timein/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import datetime
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -143,6 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = 'static/'
 
 # Default primary key field type
