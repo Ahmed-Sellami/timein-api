b02d1e0dddc3348a1b2aa6d094655ec7be9f9aeb
Ahmed-Sellami <ahmedsellami526@gmail.com>
2022-07-12 22:57:59 +0100

    Add backend url to allowed hosts

--- timein/settings.py
+++ timein/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x(3m!vjwcv67i6^9@nug5%7*zl8ylfvqb^@sz&=(xpr8vzybi!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["timein-app.herokuapp.com"]
 
 AUTH_USER_MODEL = 'authentication.User'
 
